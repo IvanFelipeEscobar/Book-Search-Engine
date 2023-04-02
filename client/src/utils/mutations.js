3609de4797a35586ca6d07204f35162d8b089136
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-02 18:21:41 -0400

    server auth reconfig

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,25 @@
 import {gql} from '@apollo/client'
 
+export const LOGIN_USER = gql`
+mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }`
+
+  export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+  `
+
+  export const SAVE_BOOK = gql`
+  `
