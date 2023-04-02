a60aae902535e16c8468e32c23d074b3a13c15f4
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-01 21:20:49 -0400

    server configuration

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const {ApolloServer} = require(`apollo-server-express`)
+
+const {typeDefs, resolvers} = require(`./schemas`)
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,3 +23,4 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+ 
