c846a982725eac640330015c405278d2a36853a2
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-02 14:29:36 -0400

    typeDefs/resolvers part 3

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,31 @@
+const {AuthenticationError} = require(`apollo-server-express`)
 const { User } = require(`../models`)
 const {signToken} = require(`../utils/auth`)
 
 const resolvers = {
     Query: {
        me: async (parent, args, context) => {
-
+       if (context.user){
+        return await User.findOne({ _id: context.user._id }).select('-__v -password')
        }
+       throw new AuthenticationError('You need to be logged in!')
+     }   
     },
     Mutation: {
-        createUser: async (parent, {username, email, password}) => {
+        addUser: async (parent, {username, email, password}) => {
             const user = await User.create({username, email, password})
+            const token = signToken(user)
+            return {token, user}
+        },
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email})
+            if(!user){
+                throw new AuthenticationError(`no user found with that email`)
+            }
+            const validPassword = await user.isCorrectPassword(password)
+            if (!validPassword) {
+                throw new AuthenticationError(`Wrong Password`)
+            }
             const token = signToken(user)
             return {token, user}
         }
