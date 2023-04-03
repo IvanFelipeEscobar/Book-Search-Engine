0e52e5c228d4a803a5e3b4c2f1330e014073eaad
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-03 11:40:45 -0400

    *

--- server/server.js
+++ server/server.js
@@ -22,9 +22,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+app.get(`*`, (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
 // app.use(routes);
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start()
