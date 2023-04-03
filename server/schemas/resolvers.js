04566b9bea355f9114ac1133450be0bb7ebf93c4
Ivan Escobar <ivnescbr1989@gmail.com>
2023-04-03 14:50:51 -0400

    debuggin again

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
     Query: {
        me: async (parent, args, context) => {
        if (context.user){
-        return await User.findOne({ _id: context.user._id }).select('-__v -password').populate(`savedBooks`)
+        return await User.findOne({ _id: context.user._id }).select('-__v -password')
        }
        throw new AuthenticationError('You need to be logged in!')
      }   
